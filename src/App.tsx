32c6dc44f6c2489c28c15aff6b6a512f06dc442d
anhphat123456789 <hunganh15610@gmail.com>
2026-01-29 22:08:31 +0700

    feat(auth): initialize auth state before rendering routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 // import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAppDispatch } from "./app/hook";
 import AppRoute from "./routes";
 import { setAuth } from "./app/features/authSlice";
 
 function App() {
   const dispatch = useAppDispatch();
+  const [isInitialized, setIsInitialized] = useState(false);
 
   useEffect(() => {
     const token = localStorage.getItem("accessToken");
@@ -16,10 +17,15 @@ function App() {
         setAuth({
           accessToken: token,
           user: JSON.parse(user),
-        })
+        }),
       );
     }
+    setIsInitialized(true);
   }, [dispatch]);
+
+  if (!isInitialized) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <>
